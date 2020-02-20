fdd5d63590b2748911a635851a8e8913e17eae9c
Luciano Oliveira Fagundes <luciano.fagundes@dbserver.com.br>
2020-02-20 14:34:16 -0300

    iniciei um projeto

---
+++ src/main/java/frameworks/BaseTestFw.java
@@ -0,0 +1,22 @@
+package frameworks;
+
+import org.junit.jupiter.api.AfterEach;
+import org.openqa.selenium.WebDriver;
+
+import static frameworks.DriverManagerFactoryFw.quitDriver;
+import static frameworks.DriverManagerFactoryFw.setDriver;
+
+public class BaseTestFw {
+
+    private WebDriver driver;
+
+    public WebDriver getDriver() {
+        this.driver = setDriver(DriverTypeFw.CHROME);
+        return driver;
+    }
+
+    @AfterEach
+    public void tearDown() {
+        quitDriver();
+    }
+}
